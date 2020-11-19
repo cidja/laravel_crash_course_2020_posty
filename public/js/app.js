82542cca7ce993a58426526a4c7150612ffc06a7
cidja <chgeorgesc@gmail.com>
2020-11-19 21:24:12 +0100

    create database migrations and use npm install

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/huclink/laravel/laravel_crash_course_2020/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/huclink/laravel/laravel_crash_course_2020/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/cidja/laravel_et_git/laravel_crash_course_2020_posty/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/cidja/laravel_et_git/laravel_crash_course_2020_posty/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,38 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Posty</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}"><!--on insère le lien css de la feuille de style avec le asset https://youtu.be/MFh0Fd7BsjE?t=893 !-->
   </head>
-  <body>
+  <body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+      <ul class="flex items-center">
+        <li>
+          <a href="#" class="p-3">Home</a>
+        </li>
+        <li>
+          <a href="#" class="p-3">Dashboard</a>
+        </li>
+        <li>
+          <a href="#" class="p-3">Post</a>
+        </li>
+      </ul>
+
+      <ul class="flex items-center">
+        <li>
+          <a href="#" class="p-3">Christian GEORGES</a>
+        </li>
+        <li>
+          <a href="#" class="p-3">Login</a>
+        </li>
+        <li>
+          <a href="#" class="p-3">Register</a>
+        </li>
+        <li>
+          <a href="#" class="p-3">Logout</a>
+        </li>
+      </ul>
+    </nav>
     @yield('content')
   </body>
 </html>
